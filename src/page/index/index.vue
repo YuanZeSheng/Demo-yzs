3b2ae03b28146ddfbb7efdb8b1646e0b2fd08fa1
yzs <yzs_615@163.com>
2017-12-16 16:38:35 +0800

    err

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -1,43 +1,10 @@
 <template>
-  <div>
-  	<header class="header">
-  		<div class="back iconfont">&#xe624;</div>
-  		<div class="search"><a href="#" class="prompt">请输入城市/景点/游戏主题</a></div>
-  		<div class="city">城市</div>
-  	</header>
-  	 <swiper :options="swiperOption">
-      <swiper-slide>
-        <div class="swiper-img-con">
-          <img  class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1609/15/630b82d932a3c402.jpg_640x200_862e836b.jpg"/>
-        </div>
-      </swiper-slide>
-      <swiper-slide>
-        <div class="swiper-img-con">
-          <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1712/5c/723bbb93e9683f02.jpg_640x200_7d38c6ad.jpg"/>
-        </div>
-      </swiper-slide>
-      <div class="swiper-pagination"  slot="pagination"></div>
-    </swiper>
-
-  </div>
 </template>
-
 <script>
-export default {
-  name: 'Index',
-  data () {
-    return {
-      swiperOption: {
-        autoplay: 1000,
-        direction: 'horizontal'
-      }
-    }
-  }
-}
+export default {name: 'Index',data () {return {swiperOption: {autoplay: 1000,direction: 'horizontal',pagination: {el: '.swiper-pagination'}}}},created () {this.getIndexData()},methods: {getIndexData () {this.$http.get('/static/index.json').then(this.handleGetDataSucc.bind(this))},handleGetDataSucc (res) {console.log(res)}}}
 </script>
 
 <style scoped>
-
 	.header{
 		display: flex;
 		height: 0.86rem;
@@ -60,13 +27,14 @@ export default {
 		width: 1.32rem;
 		line-height: .86rem;
 		text-align: left;
-		/*position: relative;*/
 		text-overflow: ellipsis;
 		white-space: nowrap;
-		
 	}
 	.swiper-img-con{
+		overflow: hidden;
 		width: 100%;
+		height: 0;
+		padding-bottom: 31.25%;
 	}
 	.swiper-img{
 		width: 100%;
@@ -78,13 +46,13 @@ export default {
 		height: 0.6rem;
 	}
 	.city:after{
-    display:block;
-    content:'';
-    border-width:0.13rem;
-    border-style:solid;
-    border-color:#FFFFFF transparent transparent transparent;
-    position:absolute;
-    top: 0.35rem;
-    right: 0.3rem;
+		display:block;
+		content:'';
+		border-width:0.13rem;
+		border-style:solid;
+		border-color:#FFFFFF transparent transparent transparent;
+		position:absolute;
+		top: 0.35rem;
+		right: 0.3rem;
 }
 </style>
